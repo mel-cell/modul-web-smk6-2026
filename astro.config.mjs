90df1d1c5cd5d684b37b0ae3f2edc0702c9d3527
mel-cell <melmel67799@gmail.com>
2026-02-09 07:41:38 +0700

    install starlight engine

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'The SMK6 Engine',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/mel-cell/modul-web-smk6-2026',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Start Here',
+          items: [{ label: 'Overview', slug: '' }],
+        },
+        {
+          label: 'Core Knowledge',
+          autogenerate: { directory: 'basic-knowledge' },
+        },
+        {
+          label: 'Architecture',
+          autogenerate: { directory: 'payload-architecture' },
+        },
+        {
+          label: 'Development',
+          autogenerate: { directory: 'development-guide' },
+        },
+      ],
+    }),
+  ],
+})
